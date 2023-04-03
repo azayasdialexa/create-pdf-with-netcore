cd1fe66a9b1e74f2bfa8848b93e9c4ae0f958933
Adam Zayas <91568235+azayasdialexa@users.noreply.github.com>
2023-04-03 13:51:26 -0500

    WIP: working version before cleanup and tweaking

--- PDF_Generator/PDF_Generator/Controllers/PdfCreatorController.cs
+++ PDF_Generator/PDF_Generator/Controllers/PdfCreatorController.cs
@@ -25,22 +25,17 @@ namespace PDF_Generator.Controllers
                 ColorMode = ColorMode.Color,
                 Orientation = Orientation.Landscape,
                 PaperSize = PaperKind.Letter,
-                PageOffset = -1,
+                PageOffset = 0,
                 Margins = new MarginSettings { Top = 0, Left = 0, Right = 0, Bottom = 0 },
                 DocumentTitle = "Availability Summary",
             };
 
             var objectSettings = new ObjectSettings
             {
-                PagesCount = true,
                 UseExternalLinks = true,
                 UseLocalLinks = true,
-                HeaderSettings = { FontName = "TTHoves", FontSize = 9, Right = "Pg [page] of [toPage]", Line = false,
-                    HtmUrl= Path.Combine(Directory.GetCurrentDirectory(), "assets", "htmlpage.html") },
                 HtmlContent = TemplateGenerator.GetHTMLString(),
-                WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "assets", "pure-css.css") },
-                FooterSettings = { FontName = "TTHoves", FontSize = 9, Right = "Pg [page] of [toPage]", Line = false,
-                    HtmUrl= Path.Combine(Directory.GetCurrentDirectory(), "assets", "footer.html") },
+                WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "assets", "style.css") },
             };
 
             var pdf = new HtmlToPdfDocument()
--- PDF_Generator/PDF_Generator/Utility/TemplateGenerator.cs
+++ PDF_Generator/PDF_Generator/Utility/TemplateGenerator.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using PDF_Generator.Models;
+using System.IO;
+using System;
 using System.Linq;
 using System.Text;
 
@@ -12,14 +16,111 @@ namespace PDF_Generator.Utility
             var availableSummaries = DataStorage.GetAllAvailable();
 
             var sb = new StringBuilder();
+            //using (StreamReader reader = new StreamReader("Template/TestHTML.html"))
+            //{
+            //    string line = "";
+            //    StringBuilder stringBuilder = new StringBuilder();
+            //    while ((line = reader.ReadLine()) != null)
+            //    {
+            //        stringBuilder.Append(line);
+            //    }
+
+            //    Console.WriteLine(stringBuilder.ToString());
+            //    Console.ReadLine();
+            //    sb = stringBuilder;
+            //}
+
             sb.Append(@"
                         <html>
                             <body>
-                                <div class='grid'>
-                                    <div class='col-4'> Column 1 </div>
-                                    <div class='col-4 alt'> Column 2 </div>
-                                    <div class='col-2'> <img /> </div>
-                                </div>");
+                                <div class='page'>
+                                    <div class='header'>
+                                        <div class='col-2'>
+                                            <div class='property-details'>
+                                                <div class='property-name-address'>
+                                                    <div class='property-name'>Clifford Drive</div>
+                                                    <div class='property-address'>9749 Clifford Drive, Dallas, TX 75220</div>
+                                                </div>
+                                                <div class='property-id-specs'>
+                                                    <div class='specs-left'>
+                                                        <table>
+                                                            <tr><td><div class='icon-24 market'/></td><td>Market:</td><td class='spacer'></td><td class='details'>Dallas/Ft Worth</td></tr>
+                                                            <tr><td><div class='icon-24 submarket'/></td><td>Submarket:</td><td class='spacer'></td><td class='details'>Dallas</td></tr>
+                                                        </table>
+                                                    </div>
+                                                    <div class='specs-right'>
+                                                        <table>
+                                                            <tr><td><div class='icon-24 clock'/></td><td>Available Date:</td><td class='spacer'></td><td class='details'>Mar 2024</td></tr>
+                                                            <tr><td><div class='icon-24 ruler'/></td><td>Available Sq Ft:</td><td class='spacer'></td><td class='details'>102,900 Sq Ft</td></tr>
+                                                        </table>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class='col-2'><div class='hero-image'></div></div>
+                                    </div>
+                                    <div class='unit-specs-container'>
+                                        <div class='unit-specs-title'>Unit Specs</div>
+                                        <div class='unit-specs-items'>
+                                            <div><span>Year Built</span><span>2015</span></div>
+                                            <div><span>Unit Drive In Door(2)</span><span>25 Doors</span></div>
+                                            <div><span>Warehouse Sq Ft</span><span>100,00 Sq Ft</span></div>
+                                            <div><span>Rail Served</span><span>No</span></div>
+                                            <div><span>Office Sq Ft</span><span>9,200 Sq Ft</span></div>
+                                            <div><span>Auto Parking</span><span>160</span></div>
+                                            <div><span>% Office</span><span>9.2%</span></div>
+                                            <div><span>Truck Parking</span><span>20</span></div>
+                                            <div><span>Clear Height</span><span>32'</span></div>
+                                            <div><span>Truck Court Depth</span><span>130'</span></div>
+                                            <div><span>Dock Door Count</span><span>28 Doors</span></div>
+                                        </div>
+                                    </div>
+                                    <div class='agent-bar'>
+                                        <table>
+                                            <tr>
+                                                <td class='av-1'><div class='avatar agent-1'></div></td>
+                                                <td class='av-2'>
+                                                    <table class='agent-details'>
+                                                        <tr><td class='agent-name'>Tracy Bertman</td></tr>
+                                                        <tr><td class='agent-title'>Broker for ABC Realty</td></tr>
+                                                    </table>
+                                                </td>
+                                                <td class='av-3'>
+                                                    <table class='agent-contact'>
+                                                        <tr><td><div class='icon-21 phone'/></td><td>(321) 123-5548)</td></tr>
+                                                        <tr><td><div class='icon-21 email'/></td><td>tbertman@linklogistics.com</td></tr>
+                                                    </table>
+                                                </td>
+                                                <td class='av-4'><div class='divider'></div></td>
+                                                <td class='av-1'><div class='avatar agent-2'></div></td>
+                                                <td class='av-2'>
+                                                    <table class='agent-details'>
+                                                        <tr><td class='agent-name'>Bill Harmen</td></tr>
+                                                        <tr><td class='agent-title'>Broker for ABC Realty</td></tr>
+                                                    </table>
+                                                </td>
+                                                <td class='av-3'>
+                                                    <table class='agent-contact'>
+                                                        <tr><td><div class='icon-21 phone'/></td><td>(321) 123-5548)</td></tr>
+                                                        <tr><td><div class='icon-21 email'/></td><td>bharmen@linklogistics.com</td></tr>
+                                                    </table>
+                                                </td>
+                                                <td class='av-4'></td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                    <div class='footer'>
+                                        <table>
+                                            <tr>
+                                                <td><div class='footer-icon'><div></td>
+                                                <td class='title'>AVAILABILITY SUMMARY</td>
+                                                <td class='page'>Page 3</td>
+                                                <td class='footer-spacer'></td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>"
+            );
 
             //foreach (var market in availableMarkets)
             //{
@@ -36,7 +137,7 @@ namespace PDF_Generator.Utility
             //                            <th>Available</th>
             //                            <th>Leasing Rep</th>
             //                        </tr>"
-            //    ,market.MarketId, market.Name);
+            //    , market.MarketId, market.Name);
 
             //    foreach (var summary in availableSummaries.Where(s => s.MarketId == market.MarketId))
             //    {
@@ -52,11 +153,9 @@ namespace PDF_Generator.Utility
             //                          </tr>", summary.Address, summary.City, summary.State, summary.TotalSF, summary.ClearHt, summary.Doors, summary.AvailableDate, summary.ListingRep);
             //    }
             //}
-                                    //</ table >
+            //sb.Append(@"</table>");
 
-            sb.Append(@"
-                                </body>
-                            </html>");
+            sb.Append(@"</div></div></body></html>");
             return sb.ToString();
         }
     }
